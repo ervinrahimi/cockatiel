b031f512dafaf9aee000ff461d5c1b9189e773ce
ervin rahimi <ervinrhimi@gmail.com>
2024-09-26 12:54:25 +0330

    initial configuration

---
+++ src/constants/routes/auth.js
@@ -0,0 +1,12 @@
+/**
+ * Auth routes constant
+ * Contains authentication related routes
+ * @type {string{}}
+ */
+
+// Main routes
+const MAIN = '/auth'
+
+export const AUTH_ROUTES = {
+  MAIN: MAIN,
+}
