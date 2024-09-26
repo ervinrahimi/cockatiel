50561e892c3297d90d0cca9550daba010127daf0
ervin rahimi <ervinrhimi@gmail.com>
2024-09-26 18:05:17 +0330

    install packages $ config auth

--- src/constants/routes/auth.js
+++ src/constants/routes/auth.js
@@ -9,4 +9,10 @@ const MAIN = '/auth'
 
 export const AUTH_ROUTES = {
   MAIN: MAIN,
+  ERROR: '/auth/error',
+  LOGIN: '/auth/login',
+  REGISTER: '/auth/register',
+  VERIFY_EMAIL: '/auth/verify-email',
+  RESET_PASSWORD: '/auth/reset-password',
+  FORGOT_PASSWORD: '/auth/forgot-password',
 }
